4692fd6300410225f981b11ce074f6a484dbb466
= <=>
2018-06-07 08:41:43 +0700

    source

---
+++ Nhan dang chu viet tay/Source/convnetjs_release/index.js
@@ -0,0 +1,14 @@
+﻿var express = require('express');
+var app = express();
+
+app.use(express.static('public'));
+app.get('/', function (req, res) {
+    res.send('Hello World2');
+})
+
+var server = app.listen(8081, function () {
+    var host = server.address().address
+    var port = server.address().port
+
+    console.log("Example app listening at http://%s:%s", host, port)
+})
